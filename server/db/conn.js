38d3404e305547fa81cb8300d514eaee660a43bc
a_rsenii <arseniisokolov10@gmail.com>
2023-01-14 01:35:49 -0300

    chore(initial): run prettier

--- server/db/conn.js
+++ server/db/conn.js
@@ -6,18 +6,18 @@ const DB_NAME = process.env.DB_NAME;
 let _db;
 
 module.exports = {
-    connectToServer: function (callback) {
-        MongoClient.connect(URL, { useNewUrlParser: true }, function (err, db) {
-            // Verify we got a good "db" object
-            if (db) {
-                _db = db.db(DB_NAME);
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
-    },
+  connectToServer: function (callback) {
+    MongoClient.connect(URL, { useNewUrlParser: true }, function (err, db) {
+      // Verify we got a good "db" object
+      if (db) {
+        _db = db.db(DB_NAME);
+        console.log("Successfully connected to MongoDB.");
+      }
+      return callback(err);
+    });
+  },
 
-    getDb: function () {
-        return _db;
-    },
+  getDb: function () {
+    return _db;
+  },
 };
