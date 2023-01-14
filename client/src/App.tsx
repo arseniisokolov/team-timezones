aa0207bee50997cf648c3537f5692ba4995a763a
a_rsenii <arseniisokolov10@gmail.com>
2023-01-14 03:20:11 -0300

    refactor(initial): env variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import { useEffect } from "react";
 import logo from "./logo.svg";
+
 import "./App.css";
 
+const { REACT_APP_HOST_NAME, REACT_APP_SERVER_PORT } = process.env;
+
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `${REACT_APP_HOST_NAME}:${REACT_APP_SERVER_PORT}/record`
+      );
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+
+      console.log(record);
+    }
+
+    fetchData();
+
+    return;
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
